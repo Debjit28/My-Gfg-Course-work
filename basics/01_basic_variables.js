41a4b38738b8f3f27c200b67f336bad9ce0a77ed
Debjit28 <debjit2807@gmail.com>
2025-07-04 16:21:47 +0530

    learning js

---
+++ basics/01_basic_variables.js
@@ -0,0 +1,15 @@
+let accountmail="Dev@gmail.vom"
+const account_id="14555"
+//account_id=0;-> not allowed no shadowing in js
+var accoountpasswd="dnf jdbfjk"
+accountcity="Jaipur"
+console.log(account_id);
+accountmail="dibb@jfnklg.com"
+accountcity="Delhi"
+console.table([account_id,accountmail,accountcity])
+
+/*
+prefer not use var
+as there is issue 
+scopes
+ */
