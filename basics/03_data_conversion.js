01958812d137d1ca65c68a5c69987049ab8da2b4
Debjit28 <debjit2807@gmail.com>
2025-07-05 12:41:57 +0530

    learning js

---
+++ basics/03_data_conversion.js
@@ -0,0 +1,30 @@
+/*
+let score = undefined;
+console.log(typeof(score));
+let value_of_num=Number(score);
+console.log(typeof(value_of_num))
+console.log(value_of_num)
+
+
+//"33"=>33
+//"33abc"=>NaN
+//true/false=>1/0
+*/
+
+
+let islogged="hit"
+let isbool=Boolean(islogged);
+console.log(isbool)
+
+/*
+1=>true 0=>false
+""=>false
+"hit"=>true
+*/
+
+let someNumber=99;
+let StringNum=String(someNumber)
+
+console.log(StringNum);
+console.log(typeof(StringNum))
+
