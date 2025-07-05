aaa5d9aae8c7bd8f26d0b08a04a64922dcbc483d
Debjit28 <debjit2807@gmail.com>
2025-07-05 17:29:37 +0530

    learning js

---
+++ homwork/homework.js
@@ -0,0 +1,10 @@
+function double_element(){
+    let arr_1=[2,3,4,5,6];
+    arr_1_len=arr_1.length;
+    for(let i=0;i<arr_1_len;i++){
+        arr_1[i]=arr_1[i]*2;
+    }
+    console.log(arr_1);
+}
+arr_1=[2,3,4,5,6,7]
+double_element(arr_1);
