303b8422d81ef0bd3b9acb74110bdf7252733aa4
Debjit28 <debjit2807@gmail.com>
2025-07-05 12:17:03 +0530

    learning js

---
+++ basics/02_data_types.js
@@ -0,0 +1,18 @@
+let name="Hitesh";
+let age=19;
+
+let islogged=false
+
+
+/*
+
+//number =>`2 to power 53
+//bigint
+//String
+// null =>stand alone
+// undefined =>
+// symbol
+// object
+// */
+
+console.log(typeof"islogged")
