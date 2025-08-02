b0ed7944d8785b0855b49a6d239c6e07c7887d01
Debjit28 <debjit2807@gmail.com>
2025-08-02 17:56:23 +0530

    Testing extension in codium

--- basics/02_data_types.js
+++ basics/02_data_types.js
@@ -22,6 +22,8 @@ let type_of_data_var_age=typeof age;
 /* boolean */
 let islogged=false;
 let type_of_data_var_islogged=typeof islogged;
+/*undefined*/
+let anime;
+let data_type_anime=typeof anime
 
-
-console.table([myname,type_of_data_var_myname,age,type_of_data_var_age,islogged,type_of_data_var_islogged])
+console.table([myname,type_of_data_var_myname,age,type_of_data_var_age,islogged,type_of_data_var_islogged,anime,data_type_anime])
